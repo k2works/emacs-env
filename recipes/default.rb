b886cdf5189bb38debb16e54dbe73cfa4809b25d
k2works <kakimomokuri@gmail.com>
2014-12-08 17:03:08 +0900

    操作

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,28 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+directory "/home/vagrant/.emacs.d" do
+  owner  "vagrant"
+  group  "vagrant"
+  mode   00700
+  action :create
+end
+
+if File.exists?("/home/vagrant/.emacs.d")
+  %w{conf elisp elpa public_repos etc info other}.each do |dir|
+    directory "/home/vagrant/.emacs.d/#{dir}" do
+      owner  "vagrant"
+      group  "vagrant"
+      mode   00700
+      action :create
+    end
+  end
+
+  cookbook_file "/home/vagrant/.emacs.d/init.el" do
+    source "emacs24/init.el"
+    owner  "vagrant"
+    group  "vagrant"    
+    mode 00644
+  end
+
+end
