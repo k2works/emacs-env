3ceb9b39b4555025c8e533e4de04023a8a90bb46
k2works <kakimomokuri@gmail.com>
2014-12-09 11:25:55 +0900

    設定

--- files/default/emacs24/init.el
+++ files/default/emacs24/init.el
@@ -13,6 +13,11 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; 環境変数の設定                                            ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; パスの設定
+(add-to-list 'exec-path "/opt/local/bin")
+(add-to-list 'exec-path "/usr/local/bin")
+(add-to-list 'exec-path "~/bin")
+
 ;; 文字コードを指定する
 (set-language-environment "Japanese")
 (prefer-coding-system 'utf-8)
@@ -45,14 +50,15 @@
 ;; リージョン内の行数と文字数をモードラインに表示する(範囲指定時のみ)
 (defun count-lines-and-chars ()
 (if mark-active
-  (format "%d ines,%d chars "
+  (format "%d lines,%d chars "
   (count-lines (region-beginning) (region-end))
-  (- (region-end) (region-beginning))
-  )
-  ))
+  (- (region-end) (region-beginning)))
+  ;; これだとエコーエリアがチラつく
+  ;;(count-lines-region (region-beginning) (region-end))
+  ""))
 
-(add-to-list 'default-mode-line-format
-  '(:eval (count-lines-and-chars)))
+  (add-to-list 'default-mode-line-format
+    '(:eval (count-lines-and-chars)))
 
 ;; タイトルバーにファイルのフルパスを表示
 (setq frame-title-format "%f")
@@ -79,35 +85,70 @@
   (color-theme-initialize))
 
 ;; フォントの設定
-;; 日本語のフォントをヒラギノ明朝Proに
-;;(set-fontset-font
-;; nil 'japanese-jisx0208
- ;; 英語名の場合
-;; (font-spec :family "Hiragino Mincho Pro"))
- ;; (font-spec :family "ヒラギノ明朝 Pro"))
+(when (eq window-system 'ns)
+ ;; asciiフォントをMenloに
+ (set-face-attribute 'default nil
+   :family "Menlo"
+   :height 120)
+   ;; 日本語フォントをヒラギノ明朝 Proに
+   (set-fontset-font
+     nil 'japanese-jisx0208
+     ;; 英語名の場合
+     ;; (font-spec :family "Hiragino Mincho Pro"))
+     (font-spec :family "ヒラギノ明朝 Pro"))
+     ;; ひらがなとカタカナをモトヤシーダに
+     ;; U+3000-303F	CJKの記号および句読点
+     ;; U+3040-309F	ひらがな
+     ;; U+30A0-30FF	カタカナ
+   (set-fontset-font
+     nil '(#x3040 . #x30ff)
+     (font-spec :family "NfMotoyaCedar"))
+     ;; フォントの横幅を調節する
+   (setq face-font-rescale-alist
+         '((".*Menlo.*" . 1.0)
+           (".*Hiragino_Mincho_Pro.*" . 1.2)
+           (".*nfmotoyacedar-bold.*" . 1.2)
+           (".*nfmotoyacedar-medium.*" . 1.2)
+           ("-cdac$" . 1.3))))
+
+(when (eq system-type 'windows-nt)
+  ;; asciiフォントをConsolasに
+  (set-face-attribute 'default nil
+                     :family "Consolas"
+                     :height 120)
+  ;; 日本語フォントをメイリオに
+  (set-fontset-font
+   nil
+   'japanese-jisx0208
+   (font-spec :family "メイリオ"))
+   ;; フォントの横幅を調節する
+   (setq face-font-rescale-alist
+         '((".*Consolas.*" . 1.0)
+           (".*メイリオ.*" . 1.15)
+           ("-cdac$" . 1.3))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ハイライトの設定                                          ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; 現在行のハイライト
+;;; 現在行のハイライト
 (defface my-hl-line-face
   ;; 背景がdarkならば背景色を紺に
   '((((class color) (background dark))
-     (:background "NavyBlue" t))
-    ;; 背景がlightならば背景色を緑に
-    (((class color) (background light))
-     (:backgournd "LightGoldenroadYellow" t))
-    (t (:bold t)))
+  (:background "NavyBlue" t))
+  ;; 背景がlightならば背景色を緑に
+  (((class color) (background light))
+  (:background "LightGoldenrodYellow" t))
+  (t (:bold t)))
   "hl-line's my face")
 (setq hl-line-face 'my-hl-line-face)
 (global-hl-line-mode t)
 
-;; 対応する括弧のハイライト
-;; paren-mode: 対応する括弧を強調して表示する
-(setq show-paren-delay 0 ) ; 表示までの秒数。初期値は0.125
-(show-paren-mode t)
+;; 括弧の対応関係のハイライト
+;; paren-mode：対応する括弧を強調して表示する
+(setq show-paren-delay 0) ; 表示までの秒数。初期値は0.125
+(show-paren-mode t) ; 有効化
 ;; parenのスタイル: expressionは括弧内も強調表示
-(setq show-apren-style 'expression)
+(setq show-paren-style 'expression)
 ;; フェイスを変更する
 (set-face-background 'show-paren-match-face nil)
 (set-face-underline-p 'show-paren-match-face "yellow")
@@ -120,17 +161,12 @@
 ;;(setq make-backup-file nil) ; 初期値はt
 ;; オートセーブファイルは作らない
 ;;(setq auto-save-default nil) ; 初期値はt
-;; バックアップファイルの作成場所をシステムのTempディレクトリに変更する
-;;(setq backup-directory-alist
-;;      '((".*" . ,temporary-file-directory)))
-;; オートセーブファイルの作成場所をシステムのTempディレクトリに変更する
-;;(setq auto-save-file-name-transforms
-;;      '((".*" ,temporary-file-directory t)))
 ;; バックアップとオートセーブファイルを~/.emacs.d/backups/へ集める
-;;(add-to-list 'backup-directory-alist
-;;             (cons "." "~/.emacs.d/backups/"))
-;;(setq auto-save-file-name-transforms
-;;      '((".*" ,(expand-file-name "~/.emacs.d/backups/") t)))
+(add-to-list 'backup-directory-alist
+  (cons "." "~/.emacs.d/backups/"))
+  (setq auto-save-file-name-transforms
+    `((".*" ,(expand-file-name "~/.emacs.d/backups/") t)))
+
 ;; オートセーブファイル作成までの秒間隔
 (setq auto-save-timeout 15)
 ;; オートセーブファイル作成までのタイプ間隔
