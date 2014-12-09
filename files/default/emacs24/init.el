47acd9326a8373288a5eec70022c656e9503a1c9
k2works <kakimomokuri@gmail.com>
2014-12-09 19:24:48 +0900

    拡張

--- files/default/emacs24/init.el
+++ files/default/emacs24/init.el
@@ -1,4 +1,4 @@
-1;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; キーバインドの設定                                         ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; C-hをバックスペースにする
@@ -355,3 +355,18 @@ ad-do-it))
   (save-buffer))
 ;; wgrepの設定
 (require 'wgrep nil t)
+
+;; さまざまな履歴管理
+;; undohistの設定
+(when (require 'undohist nil t)
+  (undohist-initialize))
+
+;; undo-treeの設定
+(when (require 'undo-tree nil t)
+  (global-undo-tree-mode))
+
+;; point-undoの設定
+(when (require 'point-undo nil t)
+  (define-key global-map (kbd "M-[") 'point-undo)
+  (define-key global-map (kbd "M-]") 'point-redo)
+  )
