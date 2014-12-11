6e682ca963cf1ff44274162027bba7dd8057724a
k2works <kakimomokuri@gmail.com>
2014-12-11 09:34:19 +0900

    デーモン起動時にC-hでバックスペースが効かない不具合対応

--- files/default/emacs24/init.el
+++ files/default/emacs24/init.el
@@ -3,7 +3,8 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; C-hをバックスペースにする
 ;; ?\C-?はDELのキーシーケンス
-(keyboard-translate ?\C-h ?\C-?)
+;;(keyboard-translate ?\C-h ?\C-?)
+(define-key key-translation-map [?\C-h] [?\C-?])
 ;; 改行と同時にインデントする
 ;; C-mにnewline-and-indentを割り当てる
 (global-set-key (kbd "C-m") 'newline-and-indent)
@@ -438,7 +439,7 @@ ad-do-it))
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("f211f8db2328fb031908c9496582e7de2ae8abd5f59a27b4c1218720a7d11803" "89127a6e23df1b1120aa61bd7984f1d5f2747cad1e700614a68bdb7df77189ba" "6394ba6170fd0bc9f24794d555fa84676d2bd5e3cfd50b3e270183223f8a6535" "ad97202c92f426a867e83060801938acf035921d5d7e78da3041a999082fb565" "31772cd378fd8267d6427cec2d02d599eee14a1b60e9b2b894dd5487bd30978e" "a3821772b5051fa49cf567af79cc4dabfcfd37a1b9236492ae4724a77f42d70d" default)))
+    ("6e03b7f86fcca5ce4e63cda5cd0da592973e30b5c5edf198eddf51db7a12b832" "3fe4861111710e42230627f38ebb8f966391eadefb8b809f4bfb8340a4e85529" "fc89666d6de5e1d75e6fe4210bd20be560a68982da7f352bd19c1033fb7583ba" "f34690262d1506627de39945e0bc2c7c47ece167edea85851bab380048dc8580" "f211f8db2328fb031908c9496582e7de2ae8abd5f59a27b4c1218720a7d11803" "89127a6e23df1b1120aa61bd7984f1d5f2747cad1e700614a68bdb7df77189ba" "6394ba6170fd0bc9f24794d555fa84676d2bd5e3cfd50b3e270183223f8a6535" "ad97202c92f426a867e83060801938acf035921d5d7e78da3041a999082fb565" "31772cd378fd8267d6427cec2d02d599eee14a1b60e9b2b894dd5487bd30978e" "a3821772b5051fa49cf567af79cc4dabfcfd37a1b9236492ae4724a77f42d70d" default)))
  '(helm-gtags-ignore-case helm)
  '(helm-gtags-path-style (quote relative))
  '(t-gtags-auto-update t)
